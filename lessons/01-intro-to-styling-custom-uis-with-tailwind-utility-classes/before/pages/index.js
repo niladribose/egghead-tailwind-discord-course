d35b3057f9a6fe857389d10704c646fbf4c4608c
niladribose <niladri1979@gmail.com>
2023-05-03 11:19:58 +0000

    just lesson things

--- lessons/01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/pages/index.js
+++ lessons/01-intro-to-styling-custom-uis-with-tailwind-utility-classes/before/pages/index.js
@@ -1,3 +1,34 @@
 export default function Home() {
-  return <p>Hello, Tailwind!</p>;
+  return (
+    <div
+      className="bg-gray-700 text-white min-h-screen flex 
+  items-center justify-center"
+    >
+      <div className="max-w-lg">
+        <div className="flex hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+          <img className="w-10 h-10 rounded-full mr-4" src="/adamwathan.jpeg" />
+          <div>
+            <p className="flex items-baseline ">
+              <span className="text-green-500 mr-2 text-sm font-medium">
+                NiladriBose
+              </span>
+              <span className="text-xs text-gray-500">01/15/2023</span>
+            </p>
+            <p className="text-gray-300 flex">
+              You should never use something like leading relaxed with a big
+              font size, it goes against all typography best practices. Line
+              height should decrease as font size gets bigger
+            </p>
+          </div>
+        </div>
+        <div className="mt-1  hover:bg-gray-800 hover:bg-opacity-30 px-4 py-1">
+          <p className="text-gray-300 pl-14">
+            You should never use something like leading relaxed with a big font
+            size, it goes against all typography best practices. Line height
+            should decrease as font size gets bigger
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- lessons/02-scaffolding-a-multi-panel-app-layout-with-flexbox/before/pages/index.js
+++ lessons/02-scaffolding-a-multi-panel-app-layout-with-flexbox/before/pages/index.js
@@ -1,3 +1,38 @@
 export default function Home() {
-  return <div>Hello, Tailwind!</div>;
+  return (
+    <div className="flex text-white h-screen">
+      <div className="bg-gray-800 p-3 space-y-2 overflow-y-scroll">
+        {[...Array(40)].map((_, i) => (
+          <div className="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
+            {i}
+          </div>
+        ))}
+      </div>
+      <div className="bg-gray-700 w-60 flex flex-col overflow-y-scroll ">
+        <div className="px-4 h-12 shadow-md flex items-center flex-shrink-0">
+          {" "}
+          Tailwind CSS
+        </div>
+        {[...Array(40)].map((_, i) => (
+          <div className="p-3 flex-1">Channels</div>
+        ))}
+      </div>
+
+      <div className="bg-gray-600 flex-1 flex flex-col overflow-y-scroll">
+        <div className="px-4 h-12 shadow-md flex items-center">general</div>
+        {[...Array(40)].map((_, i) => (
+          <div className="p-3 flex-1">
+            Messages {i} Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est
+            laborum
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
